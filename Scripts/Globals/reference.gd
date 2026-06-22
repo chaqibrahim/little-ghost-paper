6524011d7f7a7e9d588c29d53d63e642706c0203
chaqibrahim <chaqibrahim.ch@gmail.com>
2026-06-23 04:44:47 +0700

    Added some pointers

--- Scripts/Globals/reference.gd
+++ Scripts/Globals/reference.gd
@@ -7,6 +7,11 @@ extends Node2D
 @export var mainmenu_scene: PackedScene
 @export_group("Stage Scenes")
 @export var stage1_scene: PackedScene
+@export_group("Character Scenes")
+@export var player_scene: PackedScene
+@export var tutorial_guy_scene: PackedScene
+@export_group("Effect Scenes")
+@export var warp_effect: PackedScene
 
 
 func _ready() -> void:
