375af9eaa34f9702fee89cd234cd3712c041c47b
chaqibrahim <chaqibrahim.ch@gmail.com>
2026-06-22 10:35:47 +0700

    Added some plugins and some stuff
    Some plugins, resized resolution, added skip disclaimer

--- Scripts/Menus/disclaimer.gd
+++ Scripts/Menus/disclaimer.gd
@@ -2,17 +2,23 @@
 extends Control
 
 var can_continue := false
+var tween: Tween
 
 @onready var disclaimer := $Label
 
+
 func _ready() -> void:
-	var tween := create_tween()
-	tween.tween_property(disclaimer, "visible_ratio", 1.0, 10.0)
-	await tween.finished
-	can_continue = true
+    tween = create_tween()
+    tween.tween_property(disclaimer, "visible_ratio", 1.0, 10.0)
+    await tween.finished
+    can_continue = true
+
 
 func _input(event: InputEvent) -> void:
-	if not can_continue:
-		return
-	if event.is_action_pressed("ui_accept"):
-		Globals.menu.show_menu(Menu.MenuList.MAIN_MENU)
+    if event.is_action_pressed("ui_accept"):
+        if not can_continue:
+            tween.kill()
+            disclaimer.visible_ratio = 1.0
+            can_continue = true
+        else:
+            Globals.menu.show_menu(Menu.MenuList.MAIN_MENU)
