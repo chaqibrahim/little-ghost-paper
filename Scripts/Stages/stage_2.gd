e1d3535925fb2cf913c009f75045e05e421c8876
chaqibrahim <chaqibrahim.ch@gmail.com>
2026-06-25 14:15:36 +0700

    updated phantom camera

--- Scripts/Stages/stage_2.gd
+++ Scripts/Stages/stage_2.gd
@@ -17,4 +17,15 @@ func _ready() -> void:
 
 
 func walk_in() -> void:
-	pass
+	Globals.spawn.spawn_character(Spawn.SpawnList.PLAYER, player_spawn.global_position, 0.0)
+	Globals.spawn.spawn_character(Spawn.SpawnList.TUTORIAL_GUY, tutorial_spawn.global_position, 0.0)
+	await get_tree().create_timer(0.5).timeout
+	player = get_tree().get_first_node_in_group("player")
+	tutorial_guy = get_tree().get_first_node_in_group("tutorial_guy")
+
+	var tween := create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(tutorial_guy, "global_position", tutorial_walkpoint.global_position, 2.0)
+	tween.tween_property(player, "global_position", player_walkpoint.global_position, 2.0)
+	await tween.finished
+	DialogueManager.show_dialogue_balloon(dialogue, "start")
