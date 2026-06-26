a43b1a9cc649e6e0fca8bd38d0592e265296abca
chaqibrahim <chaqibrahim.ch@gmail.com>
2026-06-26 07:13:16 +0700

    changed theme fonts

--- Scripts/Components/sprite_normalizer.gd
+++ Scripts/Components/sprite_normalizer.gd
@@ -1,16 +1,29 @@
 #sprite_normalizer.gd
 extends Node
 
-enum Direction {
-	LEFT,
-	RIGHT,
-}
-
 const ROTATION_SPEED := 720.0
 
-var last_direction := Direction.RIGHT
+var sprite: Sprite2D
 
 @onready var parent: RigidBody2D = get_parent()
+
+
+func _ready() -> void:
+	for child in parent.get_children():
+		if child is Sprite2D:
+			sprite = child
+			break
+
+
+func _process(_delta: float) -> void:
+	if not sprite:
+		return
+	if parent.linear_velocity.x > 1:
+		if sprite.flip_h:
+			sprite.flip_h = false
+	elif parent.linear_velocity.x < 1:
+		if not sprite.flip_h:
+			sprite.flip_h = true
 
 
 func _physics_process(_delta: float) -> void:
